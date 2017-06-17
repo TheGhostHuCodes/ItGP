7cbe35b7547c4fcc27771f9a52b82f81aa23885a
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-06-17 08:48:43 -0700

    Make a slice and assign to the underlying array

--- hw/src/hello/main.go
+++ hw/src/hello/main.go
@@ -4,7 +4,19 @@ import (
 	"fmt"
 )
 
+func printer(words []string) {
+	for _, word := range words {
+		fmt.Printf("%s", word)
+	}
+	fmt.Printf("\n")
+}
+
 func main() {
-	words := [...]string{"the", "quick", "brown", "fox"}
-	fmt.Printf("%s\n", words[2])
+	words := make([]string, 4)
+	words[0] = "The"
+	words[1] = "Quick"
+	words[2] = "Brown"
+	words[3] = "Fox"
+	printer(words)
+	fmt.Printf("%d %d\n", len(words), cap(words))
 }
