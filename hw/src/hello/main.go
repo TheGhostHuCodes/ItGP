c6781f491f3a0e16eb1e38f45224e0c18d0f0b62
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-06-16 22:02:58 -0700

    Make variables constant instead

--- hw/src/hello/main.go
+++ hw/src/hello/main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 )
 
+const (
+	message = "The answer to life the universe and everything is %d\n"
+	answer  = 42
+)
+
 func main() {
-	message := "The answer to life the universe and everything is %d\n"
-	answer := 42
 	fmt.Printf(message, answer)
 }
