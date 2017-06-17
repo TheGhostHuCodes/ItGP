20ab2197be3a21616a0c7dd1922dd9aee99f1cbb
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-06-17 10:34:14 -0700

    Replace Errorf object with actual error object

--- hw/src/hello/main.go
+++ hw/src/hello/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
 
+var (
+	errorEmptyString = errors.New("Unwilling to print an empty string")
+)
+
 func printer(message string) error {
 	if message == "" {
-		return fmt.Errorf("Unwilling to print an empty string")
+		return errorEmptyString
 	}
 	_, err := fmt.Printf("%s\n", message)
 	return err
@@ -15,7 +20,11 @@ func printer(message string) error {
 
 func main() {
 	if err := printer(""); err != nil {
-		fmt.Printf("printer failed: %s\n", err)
+		if err == errorEmptyString {
+			fmt.Printf("You tried to print an empty string!")
+		} else {
+			fmt.Printf("printer failed: %s\n", err)
+		}
 		os.Exit(1)
 	}
 }
