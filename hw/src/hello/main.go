339ad1c9b29ac21bd0ea6d6da7161fa8ef0657ce
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-06-17 16:13:50 -0700

    Use multiple goroutines to send urls and query page lengths

--- hw/src/hello/main.go
+++ hw/src/hello/main.go
@@ -19,21 +19,35 @@ func getPage(url string) (int, error) {
 	return len(body), nil
 }
 
-func getter(url string, response chan string) {
-	length, err := getPage(url)
-	if err == nil {
-		response <- fmt.Sprintf("%s has length %d", url, length)
+func generator(url string, urlCh chan string) {
+	urlCh <- url
+}
+
+func worker(urlCh chan string, responseCh chan string, id int) {
+	for {
+		url := <-urlCh
+		length, err := getPage(url)
+		if err == nil {
+			responseCh <- fmt.Sprintf("(worker:%d)%s has length %d", id, url, length)
+		} else {
+			responseCh <- fmt.Sprintf("(worker:%d)Error getting %s: %s", id, url, err)
+		}
 	}
 }
 
 func main() {
 	urls := []string{"http://www.google.com/", "http://www.yahoo.com",
-		"http://www.bing.com", "http://bbc.co.uk"}
-	response := make(chan string)
+		"http://www.bing.com", "http://bbc.co.uk", "http://www.oreilly.com/"}
+	urlCh := make(chan string)
+	responseCh := make(chan string)
+	for i := 0; i < 10; i++ {
+		go worker(urlCh, responseCh, i)
+	}
+
 	for _, url := range urls {
-		go getter(url, response)
+		go generator(url, urlCh)
 	}
 	for i := 0; i < len(urls); i++ {
-		fmt.Println(<-response)
+		fmt.Printf("%s\n", <-responseCh)
 	}
 }
