1a07df7fbd03f0d31f2e20b239ef2d539ac517e5
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-06-16 22:00:51 -0700

    Change string to be a format string

--- hw/src/hello/main.go
+++ hw/src/hello/main.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
-	message := "Hello, World!\n"
-	fmt.Printf(message)
+	message := "The answer to life the universe and everything is %d\n"
+	answer := 42
+	fmt.Printf(message, answer)
 }
