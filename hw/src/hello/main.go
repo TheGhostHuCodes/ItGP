5b3c194a3eb1b70c481367757b0eb4f1a1d27afc
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-06-18 07:49:31 -0700

    Move shuffler interface to separate package

--- hw/src/hello/main.go
+++ hw/src/hello/main.go
@@ -2,19 +2,45 @@ package main
 
 import (
 	"fmt"
+	"shuffler"
 )
 
-func whatIsThis(i interface{}) {
-	switch v := i.(type) {
-	case string:
-		fmt.Printf("It's a string %s\n", v)
-	case uint32:
-		fmt.Printf("It's an unsigned 32-bit integer %d\n", v)
-	default:
-		fmt.Printf("Don't know %v\n", v)
-	}
+type intSlice []int
+
+func (is intSlice) Len() int {
+	return len(is)
+}
+func (is intSlice) Swap(i, j int) {
+	is[i], is[j] = is[j], is[i]
+}
+
+type weightString struct {
+	Weight int
+	s      string
+}
+
+type stringSlice []weightString
+
+func (ss stringSlice) Len() int {
+	return len(ss)
+}
+func (ss stringSlice) Swap(i, j int) {
+	ss[i], ss[j] = ss[j], ss[i]
+}
+func (ss stringSlice) Weight(i int) int {
+	return ss[i].Weight
 }
 
 func main() {
-	whatIsThis(uint32(42))
+	i := intSlice{1, 2, 3, 4, 5, 6}
+	j := stringSlice{weightString{100, "Hello"}, weightString{200, "World!"},
+		weightString{10, "Goodbye"}}
+
+	shuffler.Shuffle(i)
+	fmt.Printf("%v\n", i)
+
+	shuffler.WeightedShuffle(j)
+	fmt.Printf("%v\n", j)
+
+	fmt.Printf("Loop: %d\n", shuffler.Counter())
 }
