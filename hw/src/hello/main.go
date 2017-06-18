3a8e6e4594cdb24c742b3a7697c187255b07cefc
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-06-17 17:02:00 -0700

    Use buffered channel as a semaphore to limit work

--- hw/src/hello/main.go
+++ hw/src/hello/main.go
@@ -2,52 +2,37 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"math/rand"
+	"sync/atomic"
+	"time"
 )
 
-func getPage(url string) (int, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return 0, err
-	}
-	return len(body), nil
+var (
+	running int64 = 0
+)
+
+func work() {
+	atomic.AddInt64(&running, 1)
+	fmt.Printf("[%d ", running)
+	time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+	atomic.AddInt64(&running, -1)
+	fmt.Printf("]")
 }
 
-func generator(url string, urlCh chan string) {
-	urlCh <- url
-}
-
-func worker(urlCh chan string, responseCh chan string, id int) {
-	for {
-		url := <-urlCh
-		length, err := getPage(url)
-		if err == nil {
-			responseCh <- fmt.Sprintf("(worker:%d)%s has length %d", id, url, length)
-		} else {
-			responseCh <- fmt.Sprintf("(worker:%d)Error getting %s: %s", id, url, err)
-		}
-	}
+func worker(sema chan bool) {
+	<-sema
+	work()
+	sema <- true
 }
 
 func main() {
-	urls := []string{"http://www.google.com/", "http://www.yahoo.com",
-		"http://www.bing.com", "http://bbc.co.uk", "http://www.oreilly.com/"}
-	urlCh := make(chan string)
-	responseCh := make(chan string)
-	for i := 0; i < 10; i++ {
-		go worker(urlCh, responseCh, i)
-	}
+	sema := make(chan bool, 10)
 
-	for _, url := range urls {
-		go generator(url, urlCh)
+	for i := 0; i < 1000; i++ {
+		go worker(sema)
 	}
-	for i := 0; i < len(urls); i++ {
-		fmt.Printf("%s\n", <-responseCh)
+	for i := 0; i < cap(sema); i++ {
+		sema <- true
 	}
+	time.Sleep(30 * time.Second)
 }
