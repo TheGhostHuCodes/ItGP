adaa72bac7bd3c726938181409db584bb14f64ad
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-06-17 17:48:02 -0700

    Create type based on []int but with sum method

--- hw/src/hello/main.go
+++ hw/src/hello/main.go
@@ -2,39 +2,19 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 )
 
-type webPage struct {
-	url  string
-	body []byte
-	err  error
-}
+type SummableSlice []int
 
-func (w *webPage) isOk() bool {
-	return w.err == nil
-}
-
-func (w *webPage) get() {
-	resp, err := http.Get(w.url)
-	if err != nil {
-		w.err = err
-		return
+func (s SummableSlice) sum() int {
+	sum := 0
+	for _, i := range s {
+		sum += i
 	}
-	defer resp.Body.Close()
-	w.body, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		w.err = err
-	}
+	return sum
 }
 
 func main() {
-	w := &webPage{url: "http://www.oreilly.com/"}
-	w.get()
-	if w.isOk() {
-		fmt.Printf("URL: %s Length: %d", w.url, len(w.body))
-	} else {
-		fmt.Printf("Something went wrong: %s", w.err)
-	}
+	var s SummableSlice = SummableSlice{1, 1, 2, 3, 5, 8, 13}
+	fmt.Printf("%d\n", s.sum())
 }
