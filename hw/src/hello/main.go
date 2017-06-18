ad732b23bb98664ad2386a138ac7ec06012c3d38
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-06-17 18:00:14 -0700

    Define a shuffle interface and types that implement it

--- hw/src/hello/main.go
+++ hw/src/hello/main.go
@@ -2,19 +2,46 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
-type SummableSlice []int
+type shuffler interface {
+	Len() int
+	Swap(i, j int)
+}
 
-func (s SummableSlice) sum() int {
-	sum := 0
-	for _, i := range s {
-		sum += i
+func shuffle(s shuffler) {
+	for i := 0; i < s.Len(); i++ {
+		j := rand.Intn(s.Len() - 1)
+		s.Swap(i, j)
 	}
-	return sum
+}
+
+type intSlice []int
+
+func (is intSlice) Len() int {
+	return len(is)
+}
+
+func (is intSlice) Swap(i, j int) {
+	is[i], is[j] = is[j], is[i]
+}
+
+type stringSlice []string
+
+func (ss stringSlice) Len() int {
+	return len(ss)
+}
+
+func (ss stringSlice) Swap(i, j int) {
+	ss[i], ss[j] = ss[j], ss[i]
 }
 
 func main() {
-	var s SummableSlice = SummableSlice{1, 1, 2, 3, 5, 8, 13}
-	fmt.Printf("%d\n", s.sum())
+	is := intSlice{1, 2, 3, 4, 5, 6}
+	shuffle(is)
+	s := stringSlice{"The", "quick", "brown", "fox"}
+	shuffle(s)
+	fmt.Printf("%v\n", is)
+	fmt.Printf("%v\n", s)
 }
