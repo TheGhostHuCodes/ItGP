b89f7a1134434996d0f5765ba93c572ae0e64182
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-06-18 20:23:55 -0700

    Use poetry package to analyze poem

--- hw/src/hello/main.go
+++ hw/src/hello/main.go
@@ -2,45 +2,18 @@ package main
 
 import (
 	"fmt"
-	"shuffler"
+	"poetry"
 )
 
-type intSlice []int
-
-func (is intSlice) Len() int {
-	return len(is)
-}
-func (is intSlice) Swap(i, j int) {
-	is[i], is[j] = is[j], is[i]
-}
-
-type weightString struct {
-	Weight int
-	s      string
-}
-
-type stringSlice []weightString
-
-func (ss stringSlice) Len() int {
-	return len(ss)
-}
-func (ss stringSlice) Swap(i, j int) {
-	ss[i], ss[j] = ss[j], ss[i]
-}
-func (ss stringSlice) Weight(i int) int {
-	return ss[i].Weight
-}
-
 func main() {
-	i := intSlice{1, 2, 3, 4, 5, 6}
-	j := stringSlice{weightString{100, "Hello"}, weightString{200, "World!"},
-		weightString{10, "Goodbye"}}
 
-	shuffler.Shuffle(i)
-	fmt.Printf("%v\n", i)
-
-	shuffler.WeightedShuffle(j)
-	fmt.Printf("%v\n", j)
-
-	fmt.Printf("Loop: %d\n", shuffler.Counter())
+	p := poetry.Poem{{"And from my pillow, looking forth by light",
+		"Of moon or favouring stars, I could behold",
+		"The antechapel where the statue stood",
+		"of Newton with his prism and silent face,",
+		"The marble index of a mind for ever",
+		"Voyaging through strange seas of Thought, alone."}}
+	v, c, puncs := p.Stats()
+	fmt.Printf("Vowels: %d, Consonants: %d, Punctuation: %d\n", v, c, puncs)
+	fmt.Printf("Stanzas: %d, Lines: %d\n", p.NumStanzas(), p.NumLines())
 }
